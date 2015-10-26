6d6656f54561388ffc51dbe5b89d25803613e5bc
Sam Agnew <sagnew@eden.rutgers.edu>
2015-10-26 19:05:43 -0400

    Remove example information from makeCall.js

--- makeCall.js
+++ makeCall.js
@@ -4,10 +4,10 @@ const twilio = require('twilio');
 const client = twilio();
 
 client.makeCall({
-  url: 'http://sagnew.ngrok.io/voice',
-  to: '+16107616189',
-  from: '+16092514693',
-  statusCallback: 'http://sagnew.ngrok.io/events',
+  url: '',
+  to: '',
+  from: '',
+  statusCallback: '',
   statusCallbackMethod: 'POST',
   statusCallbackEvent: ['initiated', 'ringing', 'answered', 'completed'],
 }, (err, call) => {
